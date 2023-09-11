07fe6b62c2db85305cd8228883392445beee2731
TLibs <tlibs@hotmail.com>
2023-09-11 09:31:28 -0400

    added folder checking to file operations

--- function_library.py
+++ function_library.py
@@ -2,6 +2,8 @@ from zipfile import ZipFile
 import subprocess
 import os
 def unzipTo(zipfile, unzip_to) -> list[str]:
+	if not os.path.exists(os.path.dirname(unzip_to)):
+		os.makedirs(os.path.dirname(unzip_to))
 	zipfile_pointer = ZipFile(zipfile)
 	zipfile_pointer.extractall(unzip_to)
 	files = zipfile_pointer.namelist()
@@ -9,6 +11,8 @@ def unzipTo(zipfile, unzip_to) -> list[str]:
 	return files
 def downloadCurl(hyperlink, localFile)->dict:
 	#make sure to pad the hyperlink with https://
+	if not os.path.exists(os.path.dirname(localFile)):
+		os.makedirs(os.path.dirname(localFile))
 	if hyperlink.startswith("https://") == False:
 		hyperlink = f"https://{hyperlink}"
 	cmd = f"curl.exe -L -f -s -S --remove-on-error -o {localFile} {hyperlink}"
